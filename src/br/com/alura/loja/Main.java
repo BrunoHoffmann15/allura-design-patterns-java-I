20e4e4ab67b99b1b33154b0e88afd9bd36ddd9df
brunoshoffmann <bruno.siqueira.hoffmann@gmail.com>
2021-11-18 22:57:01 -0300

    feat: adicionando estrutura inicial

---
+++ src/br/com/alura/loja/Main.java
@@ -0,0 +1,15 @@
+package br.com.alura.loja;
+
+import br.com.alura.loja.imposto.CalculadoraDeImpostos;
+import br.com.alura.loja.imposto.TipoImposto;
+import br.com.alura.loja.orcamento.Orcamento;
+
+import java.math.BigDecimal;
+
+public class Main {
+    public static void main(String []args) {
+        Orcamento orcamento = new Orcamento(new BigDecimal("1000"));
+        CalculadoraDeImpostos calculadoraDeImpostos = new CalculadoraDeImpostos();
+        System.out.println("Imposto ICMS: " + calculadoraDeImpostos.calcular(orcamento, TipoImposto.ICMS));
+    }
+}
