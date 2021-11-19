dd27f89895b096518fc1ba1739ad456c24dd1c97
brunoshoffmann <bruno.siqueira.hoffmann@gmail.com>
2021-11-18 23:13:49 -0300

    feat: adicionando estrutura de strategy

--- src/br/com/alura/loja/Main.java
+++ src/br/com/alura/loja/Main.java
@@ -1,7 +1,8 @@
 package br.com.alura.loja;
 
 import br.com.alura.loja.imposto.CalculadoraDeImpostos;
-import br.com.alura.loja.imposto.TipoImposto;
+import br.com.alura.loja.imposto.ICMS;
+import br.com.alura.loja.imposto.ISS;
 import br.com.alura.loja.orcamento.Orcamento;
 
 import java.math.BigDecimal;
@@ -10,6 +11,6 @@ public class Main {
     public static void main(String []args) {
         Orcamento orcamento = new Orcamento(new BigDecimal("1000"));
         CalculadoraDeImpostos calculadoraDeImpostos = new CalculadoraDeImpostos();
-        System.out.println("Imposto ICMS: " + calculadoraDeImpostos.calcular(orcamento, TipoImposto.ICMS));
+        System.out.println("Imposto ICMS: " + calculadoraDeImpostos.calcular(orcamento, new ISS()));
     }
 }
